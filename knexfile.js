35f4c2fe4b200904cb1b2d2b30159893edc6995e
blaketillema <61172664+blaketillema@users.noreply.github.com>
2020-05-28 18:55:50 +1000

    idk1

---
+++ knexfile.js
@@ -0,0 +1,10 @@
+module.exports = {
+    client: 'mysql',
+    connection: {
+        host: '127.0.0.1',
+        port: '3307', //dev pc has two dbs
+        database: 'webcomputing',
+        user: 'root',
+        password: 'password'
+    }
+}
